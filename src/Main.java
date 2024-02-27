e87efae897d6162d6f4b2fae1f13b87fd6c49d26
vivaldifikry <vivaldifikry2@gmail.com>
2024-02-27 12:00:55 +0700

    commit 1 add file

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {         //nama kelas
+    public static void main(String[] args) {        //main method void
+
+        System.out.printf("Hello World!");          //output hello world
+
+    }
+}
